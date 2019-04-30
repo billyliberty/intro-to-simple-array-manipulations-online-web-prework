39bd92127adcb711109af948e7ca2b52df76530e
Joshua Crum <joshua.crum@gmail.com>
2019-04-30 15:18:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -39,7 +39,7 @@
   def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
     @list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
     @another_esoteric_language = "Malbolge"
-    @new_array = @list_of_esoteric_programming_languages.insert(6, @another_esoteric_language)
+    @new_array = @list_of_esoteric_programming_languages.insert(4, @another_esoteric_language)
   end
 
   def using_uniq
