0d61c5f30acf63e28c7de71bb9deecd5f1920d5b
Joshua Crum <joshua.crum@gmail.com>
2019-04-30 16:09:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -52,10 +52,10 @@
     new_array = @private_colleges_in_newyork.flatten
   end
 
-  def using_delete(instructors, fired)
-    instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-    fired = "Steven"
-    no_offense_steven = instructors.delete(fired)
+  
+
+  def using_delete(my_list, my_element)
+    my_list.delete(my_element)
   end
 
   def using_delete_at(famous_robots, integer)
