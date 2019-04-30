5f33bea5c3afd688b8a09c8b98562bea9dcf6568
Joshua Crum <joshua.crum@gmail.com>
2019-04-30 16:14:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,7 +11,6 @@
   end
 
   def using_pop(great_hits_of_the_nineties)
-    array = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
     great_hits_of_the_nineties.pop
   end
 
