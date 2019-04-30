8f2bc996d9c83b5ded4c3f740f9a4c49babd0a33
Joshua Crum <joshua.crum@gmail.com>
2019-04-30 15:09:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,8 +26,8 @@
   end
 
   def using_shift_with_args(ice_cream_brands)
-    ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
-    @deleted_brands = ice_cream_brands.shift(2)
+    @ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+    @deleted_brands = @ice_cream_brands.shift(2)
   end
 
   def using_concat(my_favorite_things, more_favs)
