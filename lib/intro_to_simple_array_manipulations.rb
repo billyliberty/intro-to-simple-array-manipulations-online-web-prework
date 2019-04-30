309b97bb023d8f3c27ff20c7f13648414a40cc47
Joshua Crum <joshua.crum@gmail.com>
2019-04-30 14:59:22 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,54 @@
+  def using_push (countries_in_western_africa, next_country)
+    array = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
+    next_country = "Niger"
+    countries_in_western_africa.push(next_country)
+  end
+
+  def using_unshift (neighborhoods_in_northwest_brooklyn, new_neighborhood)
+    array = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
+    new_neighborhood = "Brooklyn Heights"
+    neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
+  end
+
+  def using_pop(great_hits_of_the_nineties)
+    array = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
+    great_hits_of_the_nineties.pop
+  end
+
+  def using_shift(my_favorite_cities)
+    array = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+    my_favorite_cities.shift
+  end
+
+  def using_shift_with_args(ice_cream_brands)
+    array = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+    ice_cream_brands.shift(2)
+  end
+
+  def using_concat(my_favorite_things, more_favs)
+    my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+    more_favs = ["mario kart", "flatiron school"]
+    my_favorite_things.concat(more_favs)
+  end
+
+  def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
+    list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
+    another_esoteric_language = "Malbolge"
+    new_array = list_of_esoteric_programming_languages.insert(another_esoteric_language)
+  end
+
+  def using_uniq
+
+  end
+
+  def using_flatten
+
+  end
+
+  def using_delete
+
+  end
+
+  def using_delete_at
+
+  end
