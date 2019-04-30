138655ba81e6f123f6af1ab94ce6e215c7af9f55
Joshua Crum <joshua.crum@gmail.com>
2019-04-30 15:43:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -31,11 +31,11 @@
   end
 
   def using_concat(my_favorite_things, more_favs)
-    my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+    @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
     more_favs = ["mario kart", "flatiron school"]
-    my_favorite_things.concat(more_favs)
+    @all_my_favs = @my_favorite_things.concat(more_favs)
   end
-
+  
   def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
     list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
     another_esoteric_language = "Malbolge"
